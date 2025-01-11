6f63f6a6a3c99ce17cffa906a6a294e0d9e936e2
kikidmx <168574112+kikidmx@users.noreply.github.com>
2025-01-11 14:21:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
+module.exports = { session: process.env.SESSION_ID || 'BWM-XMD;;;H4sIAAAAAAAAA5VUW4+iSBj9L/WqPS2IICadLCDiDQXBC2wmmwIKKLmVBajY8b9PaLvT/bAz28tTBahTp87lewV5gUu0QA0YvQJC8RlWqF1WDUFgBOQ6DBEFXRDACoIRkCTdnZC+69aGXYllUrrHo1XEu541FDqLxuZcW+hbBR7vnRdw7wJSeyn2/wDoBbzKh02nHrAOZUzm2VsZ0sHUererO48Uq6LWPCz2rMFyL+DeIkJMcR6pJEYZojBdoMaAmH6PvjMJx8tOjDGWnJsrHNTsUspQO9s2w7s3btxM+nCOJsJtq3+PvusIobnZbeZj/TndzNZEXCTOXJouGIUTYVGymW+bkkWL7exBv8RRjoJZgPIKV823dW/G4mEilmyzMpCvWqyTwNCg+vPCjVPx6HOns9wLpMmlnhTfI45VbzNHz1k0DkW1JGNiXi15H/hFLqA1u7aqoLkWyqQDp72vxA36kZXk/+ieyZsFWR73ep5tArFYz6u67BhnOp2IlzjJLsi5sdhR63yvfo++1Rdqf4y988LNdROtPJfyHDpa9RmSIWXmSdaTDtvhbjCWPunDqqZ/FHmhznqZFBvOEe+vM75DGvsgDHE6dfeWQJhzpj9zqz1v6J623dWmd9GI09HL2p50VnEsuCxWDjPGjjZu0pyS6fj4HCvRy9uNEtTMAjBi7l1AUYTLisIKF/nbO4brAhicLeRTVL3JC1ISLYqSxanKFOeQzrdxSi4zblnTrZ55mkOs5ZFuVH5WJC+gCwgtfFSWKJjisipoo6OyhBEqwejvn12Qo2v1MK49jme6IMS0rLZ5TdICBh+ufnyEvl/UeWU1ua+0C0TBqPf5GlUVzqOy1bHOIfVjfEZKDKsSjEKYlujeBQE6Yx+1eIDNcO8fex1aDpQva29QppZbRy3luMgfv4RD3ud8iJ4Y0R8+cT1++ORBVngKBz3Y91mPFwcC6AL83pl2z28tPDjC3td2hy1/iOZDvtxoxs3jU+uiP+bRQ3tEUQBGFa1RF3jQT2piFwnK/4CLnBWbK6tsOixlZhPdyBIuzXQlmNPLF9yHp2D0+jmnlCJo8RacbS44kwVdkL1FELc353oCywz6HMfzoz7/V/nj0uoICfmRo6qFfde83RCgCuK0BCOgzLVOTGayujIU7F00TVIjSYkk8OnRR9gfYVIMqfFnDnuq1+rpGA7sHbbkftJPMlkh9KBMWUPU1zs2j4uXfwEBI1AowYkqYnFIIi2Wvet2hw63k2rQa75x5IEMXeLlUhGoPbmTTbwU1hVNNRGFJ/Z0k1eTvinK1vnQIRxnzBmtsMdSILfN+MjK18O8YCUWN//WWSiz8WrCpOOBUarudtc5LPVZR0KaLW/iAXFgY1wC0gS2L/LKKd/hmQmttXSmfl/Ukolhr2dMMOxfybYvm48avo2B9H384reCvL4nK8TobZrlsHXoP7z5mvHevfsF4n08/iZHsnm86LmirU70GC+KfmfLOEkUDFaDeWSeIHOJxeOzPU4DwkFwv//sApLCKixoBkYA5gEtcAC6IIVlJX3W1MYZKiuYETBihD4/EIXhgO+CrJEIsSpYfbQbSO2zvG3B/RdBpL/v+QcAAA==',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Ibrahim Adams",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " Ibrahim Adams",              
+    OWNER_NAME: process.env.OWNER_NAME || "kiki",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "0721534466",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || 'BMW_MD',
@@ -21,7 +21,7 @@ module.exports = { session: process.env.SESSION_ID || '',
     HEROKU_APY_KEY : process.env.HEROKU_APY_KEY ,
     WARN_COUNT : process.env.WARN_COUNT || '3' ,
     ETAT : process.env.PRESENCE || '',
-    CHATBOT : process.env.PM_CHATBOT || 'no',
+    CHATBOT : process.env.PM_CHATBOT || 'yes',
     DP : process.env.STARTING_BOT_MESSAGE || "yes",
     ANTIDELETE1 : process.env.ANTIDELETE1 || 'yes',
     ANTIDELETE2 : process.env.ANTIDELETE2 || 'yes',
